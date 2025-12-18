096af5000f5a1303700148ad99c68136a9e06f98
kerols04 <116015613+kerols04@users.noreply.github.com>
2025-12-18 01:47:03 +0100

    Update MC.cs

--- PragueParkingV2-master/PragueParkingV2.Core/MC.cs
+++ PragueParkingV2-master/PragueParkingV2.Core/MC.cs
@@ -1,30 +1,21 @@
-﻿using System;
+using System;
 
 namespace PragueParkingV2.Core
 {
-    // Motorcykel klassen ärver från Vehicle, så den får alla grundfunktioner.
     public class MC : Vehicle
     {
-        // Konstruktor: Körs när en ny MC skapas (anropar basklassens konstruktor först)
         public MC(string regNo) : base(regNo)
         {
-            Size = 1; // En MC tar bara 1 enhet plats
+            Size = GarageSettings.Current.GetVehicleSize("MC", fallback: 1);
         }
-        // Tom konstruktor: Behövs för att kunna läsa in sparad data från JSON-filen
+
         public MC() : base()
         {
-            Size = 1;
+            Size = GarageSettings.Current.GetVehicleSize("MC", fallback: 1);
         }
-        // Här implementerar MC klassen den obligatoriska funktionen för timpris
-        public override decimal GetHourlyRate()
-        {
-            // Hämtar timpriset för MC från prislistan
-            return PriceList.GetPrice("MC");
-        }
-        // Returnerar ett läsbart namn för fordonstypen
-        public override string GetVehicleTypeName()
-        {
-            return "Motorcykel";
-        }
+
+        public override decimal GetHourlyRate() => PriceList.GetPrice("MC");
+
+        public override string GetVehicleTypeName() => "Motorcykel";
     }
 }
